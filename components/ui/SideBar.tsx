18471937f11efb087de549a5a91b3b512469ee54
unknown <abiabhi2712@gmail.com>
2025-07-15 20:48:26 +0530

    Implement SideBars

--- components/ui/SideBar.tsx
+++ components/ui/SideBar.tsx
@@ -1,8 +1,15 @@
+"use client"
+
 import React from 'react'
 import Link from 'next/link'
 import Image from 'next/image'
+import { sidebarLinks } from '@/constants'
+import { cn } from '@/lib/utils'
+import { usePathname } from 'next/navigation'
 
 const SideBar = ({user}: SiderbarProps) => {
+  const pathname = usePathname();
+  
   return (
     <section className="sidebar">
         <nav className="flex flex-col gap-4">
@@ -13,10 +20,32 @@ const SideBar = ({user}: SiderbarProps) => {
                 src="/icons/logo.svg" 
                 width = {34}
                 height = {34}
+                className='size-[24px] max-xl:size-14'
                 />
-
+  <h1 className='sidebar-logo'>FinTrak</h1>
                
             </Link>
+            {sidebarLinks.map((item) =>
+          {
+              const isActive = pathname === item.route
+            || pathname.startsWith(`${item.route}/`)
+
+            return(<Link href={item.route} 
+            key={item.label}
+            className={cn('sidebar-link', 
+              {'bg-bank gradient': isActive})}
+            >
+            <div className='relative size-6'>
+              <Image src={item.imgURL}
+              alt={item.label}
+              fill
+              />
+
+              
+            </div>
+            </Link>)
+          }
+          )}
         </nav>
     </section>
   )
